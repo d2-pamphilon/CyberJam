ea42869e3b446e5751976c855c18dbd3ddd423e6
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-05-12 17:59:41 +0100

    Made USB

---
+++ CyberSecVR/Assets/Scripts/UsbGrabber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsbGrabber : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<UsbProgram>())
+        {
+            other.gameObject.transform.position = transform.FindChild("USB WAYPOINT").transform.position;
+            other.gameObject.transform.rotation = transform.FindChild("USB WAYPOINT").transform.rotation;
+        }
+
+    }
+}
