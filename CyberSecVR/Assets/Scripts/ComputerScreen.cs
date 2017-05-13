1b3a52aa34bb974ca748eaa9e3ce80f2873115cd
Nickstu14 <nickstu14@hotmail.co.uk>
2017-05-13 16:09:12 +0100

    Computer now has a UI ish

---
+++ CyberSecVR/Assets/Scripts/ComputerScreen.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ComputerScreen
+{
+    public class ComputerScreen : MonoBehaviour
+    {
+
+        public UsbProgram.Program m_virus;
+
+        private MeshRenderer m_Renderscreen;
+
+        public List<Material> m_ScreenMat;
+        public List<Sprite> m_sprites;
+
+        private Canvas m_Canvas;
+        private UnityEngine.UI.Image m_Image;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_virus = UsbProgram.Program.NONE;
+            m_Renderscreen = GetComponent<MeshRenderer>();
+            m_Canvas = GetComponentInChildren<Canvas>();
+            m_Image = GetComponentInChildren<UnityEngine.UI.Image>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if (m_virus == UsbProgram.Program.PhishingAttack)
+          
+            switch (m_virus)
+            {
+                case UsbProgram.Program.PhishingAttack:
+                    m_Canvas.enabled = true;
+                    m_Image.sprite = m_sprites[0];
+                    break;
+                case UsbProgram.Program.TrojanHorse:
+                    m_Canvas.enabled = true;
+                    m_Image.sprite = m_sprites[1];
+                    break;
+                default:
+                    m_Canvas.enabled = false;
+                    break;
+            }
+        }
+
+
+        public void ChangeScreen(UsbProgram.Program _Virus)
+        {
+            m_virus = _Virus;
+        }
+
+
+
+    }
+}
