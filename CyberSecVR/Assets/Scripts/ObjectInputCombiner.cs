33be7ab9774668e87ee592e3d6521647778009c5
gualtyphone <gualtyphone@me.com>
2017-05-13 11:48:10 +0100

    Not much

--- CyberSecVR/Assets/Scripts/ObjectInputCombiner.cs
+++ CyberSecVR/Assets/Scripts/ObjectInputCombiner.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class ObjectInputCombiner : MonoBehaviour {
 
-	void OnCollisionEnter (Collision other)
-	{
-		CombinableObject cObj = other.gameObject.GetComponentInChildren<CombinableObject>();
-		if (cObj)
-		{
-			GetComponentInParent<Blender>().addObject(cObj.type);
-			Destroy(other.gameObject);
-		}
-	}
-
     void OnTriggerEnter(Collider other)
     {
         CombinableObject cObj = other.gameObject.GetComponentInChildren<CombinableObject>();
+        CombinableObject cObj2 = other.gameObject.GetComponent<CombinableObject>();
+        CombinableObject cObj3 = other.gameObject.GetComponentInParent<CombinableObject>();
         if (cObj)
         {
             GetComponentInParent<Blender>().addObject(cObj.type);
+            Destroy(other.gameObject);
+        }
+        else if (cObj2)
+        {
+            GetComponentInParent<Blender>().addObject(cObj2.type);
+            Destroy(other.gameObject);
+        }
+        else if (cObj3)
+        {
+            GetComponentInParent<Blender>().addObject(cObj3.type);
             Destroy(other.gameObject);
         }
     }
